ee46feb917eaca74fe3940cd740007f425ba94e9
Lin Huan Po <foxs91092@gmail.com>
2020-10-24 16:10:00 +0800

    ARC 105 PC - use DP instead of SSSP

--- arc105/C.cpp
+++ arc105/C.cpp
@@ -18,108 +18,6 @@ using MLL = map <long long, long long>;
 
 const long long INF = 1000000000000000LL;
 
-template <typename T>
-struct Edge {
-    int to;
-    T w;
-
-    Edge() {}
-    Edge(int pt, T pw) : to(pt), w(pw) { }
-};
-
-template <typename T>
-class SPFA {
-    using VE = vector<Edge<T>>;
-    using VVE = vector<VE>;
-    using VI = vector <int>;
-    using VT = vector <T>;
-    // const T INF = 1000000000;
-
-    int V;
-    VI prev;
-    VVE adja;
-    VT dist;
-
-public:
-    SPFA(int PV) {
-        V = PV;
-        adja = VVE(V);
-        prev = VI(V, -1);
-        dist = VT(V, INF);
-    }
-
-    bool sssp(int src) {
-        deque <int> que;
-        VI inQue(V);
-        VI prevTime(V);
-
-        dist[src] = 0;
-        inQue[src] = 1;
-        que.push_back(src);
-
-        while (!que.empty()) {
-            int now = que.front();
-            que.pop_front();
-
-            inQue[now] = 0;
-
-            for (int i = 0; i < adja[now].size(); ++i) {
-                int nextv = adja[now][i].to;
-                T newdist = dist[now] + adja[now][i].w;
-
-                if (newdist < dist[nextv]) {
-                    dist[nextv] = newdist;
-                    prev[nextv] = now;
-                    prevTime[nextv] = prevTime[now] + 1;
-
-                    if (prevTime[nextv] == V) {
-                        return true; // negative cycle
-                    }
-
-                    if (inQue[nextv] == 0) {
-
-                        if (que.empty() || dist[nextv] >= dist[que.front()]) {
-                            que.push_back(nextv);
-                        } else {
-                            que.push_front(nextv);
-                        }
-
-                        inQue[nextv] = 1;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
-    T getBest(int v) {
-        return dist[v];
-    }
-
-    void addEdge(int f, int t, T w) {
-        adja[f].emplace_back(Edge(t, w));
-    }
-
-    vector <int> findPath(int src, int dest) {
-        vector <int> ret;
-        int now = dest;
-
-        while (now != -1) {
-            ret.push_back(now);
-            now = prev[now];
-        }
-
-        if (!ret.empty() && ret.back() != src) { // src can't connect to dest
-            ret.clear();
-        } else {
-            reverse(ret.begin(), ret.end());
-        }
-
-        return ret;
-    }
-};
-
 void solve(int now, MLL& wmap, VI& used, VI& order, VLL& w, long long& ans) {
     if (now == w.size()) {
         int N = w.size();
@@ -131,7 +29,6 @@ void solve(int now, MLL& wmap, VI& used, VI& order, VLL& w, long long& ans) {
             wsum[i] = wsum[i-1] + w[order[i]];
         }
 
-        SPFA<long long> g(N);
         VLL dp(N);
 
         for (int i = 0; i < N; ++i) {
@@ -143,17 +40,11 @@ void solve(int now, MLL& wmap, VI& used, VI& order, VLL& w, long long& ans) {
                 long long leng = iter->second;
                 // dist[i] - dist[j] >= leng
                 // -leng >= dist[j] - dist[i]
-                g.addEdge(i, j, -leng);
-                // dp[j] = max(dp[j], dp[i]+leng);
+                dp[j] = max(dp[j], dp[i]+leng);
             }
         }
 
-        if (!g.sssp(0)) { // no negative cycle
-            long long res = g.getBest(N-1) - g.getBest(0);
-            ans = min(ans, -res);
-        }
-
-        // ans = min(ans, dp[N-1]);
+        ans = min(ans, dp[N-1]);
 
         return ;
     }
