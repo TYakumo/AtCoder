ae8fb4f6ae998631320690c191218c01e26f7db3
Lin Huan Po <foxs91092@gmail.com>
2021-05-30 22:44:15 +0900

    ABC 203 competed on 30th May 2021

---
+++ abc203/B.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+using VI = vector <int>;
+using VVI = vector <VI>;
+using VVVI = vector <VVI>;
+using VLL = vector <long long>;
+using VVLL = vector <VLL>;
+using UMI = unordered_map<int, int>;
+using UMLL = unordered_map<long long, long long>;
+
+
+int main() {
+    int N;
+    int K;
+    cin >> N >> K;
+
+    int sum = K*(K+1)/2*N;
+    sum += N*(N+1)/2*K*100;
+
+    cout << sum << endl;
+
+    return 0;
+}
